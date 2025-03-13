7a9952afbe943e194daea39cef60e1b4deb50a2e
MayaTh <maya.theodorou@interweaveagency.gr>
2025-03-13 23:08:59 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function Example() {
   };
 
   for (const key in products) {
-    total += parseFloat(products[key].price.toFixed(2));
+    total += parseFloat(products[key].price);
   }
 
   return (
@@ -98,7 +98,7 @@ export default function Example() {
                   <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                     <div className="flex justify-between text-base font-medium text-gray-900">
                       <p>Subtotal</p>
-                      <p>€{total}</p>
+                      <p>€{total.toFixed(2)}</p>
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">
                       Shipping and taxes calculated at checkout.
